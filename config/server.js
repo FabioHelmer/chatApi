c8ab9a655f63dff8f4f678906942ad07c07754c5
fabio.kuh <fabio.kuhn@el.com.br>
2020-01-10 20:44:18 -0300

    correção require

--- config/server.js
+++ config/server.js
@@ -17,7 +17,8 @@ app.use(cors());
 app.use(express.static("app/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-
+app.use(require("./app/controllers"));
+app.use(require("./app/routes"));
 //autoload
 consign()
   .include("app/routes")

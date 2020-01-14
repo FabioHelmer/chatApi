de93b4d39ee108ac513bb8dff6e37b4711bb374c
fabio.kuh <fabio.kuhn@el.com.br>
2020-01-13 21:45:29 -0300

    correções controllers

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,6 +1,6 @@
 module.exports = aplication => {
   aplication.post("/chat", (req, res) => {
-    aplication.app.controllers.Chat.chat_init(aplication, req, res);
+    aplication.controllers.Chat.chat_init(aplication, req, res);
     console.log(aplication.app.controllers);
   });
   aplication.get("/chat", (req, res) => {
